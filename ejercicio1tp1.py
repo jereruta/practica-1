f5386b4fb771f3b4d5bc3251c2d6e3f1ae24be2b
Jere Ruta <rutajere@gmail.com>
2023-03-16 17:47:14 -0300

    subo el tp1

---
+++ ejercicio1tp1.py
@@ -0,0 +1,43 @@
+from random import choice, randrange
+from datetime import datetime
+# Operadores posibles
+operators = ["+", "-","*","/"]
+cont = 0
+# Cantidad de cuentas a resolver
+times = 5
+# Contador inicial de tiempo.
+# Esto toma la fecha y hora actual.
+init_time = datetime.now()
+print(f"¡Veremos cuanto tardas en responder estas {times} operaciones!")
+for i in range(0, times):
+    # Se eligen números y operador al azar
+    number_1 = randrange(10)
+    number_2 = randrange(10)
+    operator = choice(operators)
+    # Se imprime la cuenta.
+    print(f"{i+1}- ¿Cuánto es {number_1} {operator} {number_2}?")
+    # Le pedimos al usuario el resultado
+    result = float(input("resultado: "))
+    if operator == "+":
+        result2 = number_1 + number_2
+    elif operator == "-":
+        result2 = number_1 - number_2
+    elif operator == "*":
+        result2 = number_1 * number_2
+    elif operator == "/" and number_2 == 0:
+        print(f"me autoboicotie y quiero divir por cero, tu suerte le gano a la matrix!")
+    else :
+        result2 = number_1 / number_2
+
+    if result == result2 :
+        print(f"uste posee la verdad")
+        cont = cont + 1
+    else :
+        print(f"la verdad es {result2}")
+    # Al terminar toda la cantidad de cuentas por resolver.
+# Se vuelve a tomar la fecha y la hora.
+end_time = datetime.now()
+# Restando las fechas obtenemos el tiempo transcurrido.
+total_time = end_time - init_time
+# Mostramos ese tiempo en segundos.
+print(f"\n Tardaste {total_time.seconds} segundos y tuviste {cont} verdades universales")
